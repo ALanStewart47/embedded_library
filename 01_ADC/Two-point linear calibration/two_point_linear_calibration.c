e1abb6d0ccdb24e2d5a2b4d84051663ee60a5d53
ALanStewart47 <843530433@qq.com>
2026-05-28 14:58:15 +0800

    feat:adc calibration

---
+++ 01_ADC/Two-point linear calibration/two_point_linear_calibration.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * @file two_point_linear_calibration.c
+ *
+ * @par dependencies
+ * - two_point_linear_calibration.h
+ *
+ * @author Alan 
+ *
+ * @brief Provide the HAL APIs of the adc value calibration.
+ *
+ * Processing flow:
+ * call directly.
+ *
+ * @version V1.0        2026-05-28  *
+ * @note    
+ *       1 tab == 4 spaces!
+ *       encoding == UTF-8
+ *
+ *****************************************************************************/
+
+#include 
+
---
+++ 01_ADC/Two-point linear calibration/two_point_linear_calibration.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * @file two_point_linear_calibration.h
+ *
+ * @author Alan 
+ *
+ * @brief Provide the HAL APIs of the adc value calibration.
+ *
+ * Processing flow:
+ * call directly.
+ *
+ * @version V1.0        2026-05-28  *
+ * @note    
+ *       1 tab == 4 spaces!
+ *       encoding == UTF-8
+ *
+ *****************************************************************************/
+#ifndef __TWO_POINT_LINEAR_CALIBRATION_H__
+#define __TWO_POINT_LINEAR_CALIBRATION_H__
+
